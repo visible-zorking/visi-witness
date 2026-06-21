86112cdce5b705ad9f4115b788d36f04ca999531
Andrew Plotkin <zarf@ZarfLent.local>
2026-06-21 00:08:02 -0400

    Game-specific type display.

--- src/custom/cwidgets.tsx
+++ src/custom/cwidgets.tsx
@@ -3,8 +3,9 @@ import { useState, useContext, createContext } from 'react';
 
 import { ZStatePlus, ZObject } from '../visi/zstate';
 import { ObjectData, GlobalData } from '../visi/gametypes';
-import { StackCallCtx } from '../visi/context';
+import { ReactCtx, StackCallCtx } from '../visi/context';
 import { ArgShowObject, ArgShowProperty } from '../visi/actshowers';
+import { VarShowObject, VarShowProperty } from '../visi/globshow';
 import { gamedat_ids, gamedat_distances, gamedat_object_treesort } from '../visi/gamedat';
 
 export function contains_label(obj: ObjectData) : string
@@ -67,11 +68,52 @@ export function ObjListSorter({ followKey, setFollowKey } : { followKey:number, 
 
 export function global_value_display(tag: string, value: number, glo: GlobalData) : JSX.Element|null
 {
+    switch (tag) {
+        
+    case 'PRSO':
+        let rctx = useContext(ReactCtx);
+        if (rctx.zstate.globals[121] == 125) {  /* PRSA == WALK */
+            return (
+                <VarShowProperty value={ value } />
+            )
+        }
+        return (
+            <VarShowObject value={ value } />
+        )
+        
+    case 'HMTIME':
+        return (
+            <ArgShowTime value={ value } />
+        );
+        
+    case 'HMTIMEZ':
+        if (value == 0) {
+            return <i>not set</i>;
+        }
+        return (
+            <ArgShowTime value={ value } />
+        );
+        
+    }
+    
     return null;
 }
 
 export function property_value_display(tag: string, values: number[]) : JSX.Element|null
 {
+    switch (tag) {
+        
+    case 'CORBITS':
+        return (
+            <VarShowCorridorBits value={ values[0]*0x100+values[1] } />
+        )
+        
+    case 'TLINE':
+        return (
+            <VarShowTLine value={ values[1] } />
+        )
+    }
+    
     return null;
 }
 
@@ -81,7 +123,7 @@ export function stack_call_arg_display(tag: string, value: number) : JSX.Element
         
     case 'PERFORMO':
         let ctx = useContext(StackCallCtx);
-        if (ctx.args[0] == 999) {      /* action WALK */
+        if (ctx.args[0] == 125) {      /* action WALK */
             return (
                 <ArgShowProperty value={ value } />
             );
@@ -94,8 +136,77 @@ export function stack_call_arg_display(tag: string, value: number) : JSX.Element
         return (
             <ArgShowObject value={ value } />
         )
+        
+    case 'HMTIME':
+        return (
+            <ArgShowTime value={ value } />
+        )
     }
 
     return null;
 }
 
+
+export function VarShowCorridorBits({ value }: { value:number })
+{
+    let ls: string[] = [];
+
+    for (let bit=1; bit < 65536; bit *= 2) {
+        if (value & bit)
+            ls.push(''+bit);
+    }
+
+    if (!ls.length)
+        ls.push('0');
+    
+    let str = ls.join(',');
+    
+    return (
+        <i>cor-{ str }</i>
+    );
+}
+
+export function VarShowTLine({ value }: { value:number })
+{
+    let val: string;
+    switch (value) {
+    case 1:
+        val = ',INSIDE-LINE-C';
+        break;
+    case 2:
+        val = ',OFFICE-LINE-C';
+        break;
+    case 3:
+        val = ',MONICA-LINE-C';
+        break;
+    case 4:
+        val = ',OUTSIDE-LINE-C';
+        break;
+    default:
+        return null;
+    }
+    return (
+        <code>{ val }</code>
+    );
+}
+
+export function ArgShowTime({ value }: { value:number })
+{
+    let minutes = value % 60;
+    let hours = Math.floor(value / 60);
+
+    let ampm = (hours < 12) ? 'am' : 'pm';
+    if (hours == 0) {
+        hours = 12;
+    }
+    else if (hours > 12) {
+        hours -= 12;
+    }
+    let strmin = ''+minutes;
+    if (minutes < 10)
+        strmin = '0'+minutes;
+
+    return (
+        <i>{ hours }:{ strmin }{ ampm }</i>
+    )
+}
